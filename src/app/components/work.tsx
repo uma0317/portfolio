8069eae62e6ef61ff747b987ab52a2bc90df60f1
uma0317 <blog3170@gmail.com>
2023-09-05 04:42:02 +0900

    first commit

---
+++ src/app/components/work.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+type Props = {
+    title: string
+    tags: string[]
+    imagePath?: string
+    alt?: string
+    iconType?: string
+    url?: string
+    children: React.ReactNode
+}
+
+
+export default function Work({ title, tags, imagePath, alt, iconType, url, children }: Props) {
+    const tagsEl = tags.map(tag => {
+        return <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 mr-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span>
+    })
+    return (
+        <li>
+            <div className="flex items-center gap-x-6">
+                {(imagePath && alt) && <Image
+                    className="h-32 w-32 object-cover object-center"
+                    src={imagePath}
+                    width={100}
+                    height={100}
+                    alt={alt}
+                />}
+
+                {iconType === "Github" && <svg className="w-32 h-32 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
+                </svg>}
+
+                {iconType === "twitter" && <svg className="w-32 h-32 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" d="M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z" />
+                </svg>}
+
+                <div>
+                    {url
+                        ? <a href={url} target='blank' className='flex justify-start items-center'>
+                            <h3 className="text-base font-semibold leading-7 tracking-tight text-white-900 mr-1">
+                                {title}
+                            </h3>
+                            <svg className="w-3 h-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                <path d="M17 0h-5.768a1 1 0 1 0 0 2h3.354L8.4 8.182A1.003 1.003 0 1 0 9.818 9.6L16 3.414v3.354a1 1 0 0 0 2 0V1a1 1 0 0 0-1-1Z" />
+                                <path d="m14.258 7.985-3.025 3.025A3 3 0 1 1 6.99 6.768l3.026-3.026A3.01 3.01 0 0 1 8.411 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V9.589a3.011 3.011 0 0 1-1.742-1.604Z" />
+                            </svg>
+                        </a>
+                        : <h3 className="text-base font-semibold leading-7 tracking-tight text-white-900">{title}</h3>
+                    }
+
+                    {tagsEl}
+                    <p className="mt-1 text-xs leading-5 text-white-500">
+                        {children}
+                    </p>
+                </div>
+            </div>
+        </li>
+    )
+}
---
+++ src/app/works/page.tsx
@@ -0,0 +1,109 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Work from '../components/work'
+
+export default function Home() {
+    return (
+        <div className="bg-black py-24 sm:py-32">
+            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+                <div className="max-w-2xl">
+                    <h2 className="text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">My Works</h2>
+                    <p className="mt-6 text-lg leading-8 text-white-600">
+                        趣味で開発してきたモノたち<br />
+                        プライベートリポジトリのモノもあります<br />
+                    </p>
+                </div>
+
+                <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+                    <Work title='neos' tags={["React", "TypeScript", "VScode"]} imagePath='/works/neos.png' alt="neos">
+                        感情曲線と脚本を簡単に記述するためのツール。
+                    </Work>
+                    <Work title='Neoki Beat' tags={["UE5", "MIDI"]} imagePath='/works/sound_game.png' alt="sound_game">
+                        自作の音ゲー。MIDI で譜面データを作成し、UE5上で読み込んでいます。
+                    </Work>
+                    <Work title='All Dash' tags={["UE5"]} imagePath='/works/alldash.png' alt="alldash">
+                        シンプルな2D横スクロールアクションゲーム。T-Rex Dinosaur Gameのような感じです。
+                    </Work>
+                    <Work title='Keiba' tags={["UE5", "blender", "Node.js"]} imagePath='/works/keiba.png' alt="keiba">
+                        競馬風レースゲーム。(Todo)Youtube チャットをストリーミングしてリアルタイムで参加・投票ができるようにする予定です。
+                    </Work>
+                    <Work title='bl2pa' tags={["Minecraft", "blender", "Node.js", "Python"]} imagePath='/works/bl2pa.png' alt="bl2pa">
+                        blender で 3Dモデルを点群に変換しPythonで頂点およびカラー情報を計算、その後パーティクルコマンドに変換しMinecraft で再生するツール。複雑なアニメーションにも対応。
+                    </Work>
+                    <Work title='particle-video' tags={["Minecraft", "Node.js"]} imagePath='/works/particle_video.png' alt="particle-video">
+                        画像や動画を Minecraft のパーティクルコマンドに変換し、Minecraft 上で再生するツール。
+                    </Work>
+                    <Work title='自動字幕生成ツール' tags={["Python", "Lua", "Davinci Resolve", "Whisper"]} iconType='Github'>
+                        Whisperで文字起こしをして、Davicni Resolve のテキストを自動で生成するスクリプト
+                    </Work>
+                    <Work title='Audio Reactive' tags={["UE5", "blender"]} imagePath='/works/ue_sound_minecraft.png' alt="ue_sound_minecraft">
+                        UE5 でサウンドに合わせてオブジェクトを生成し、その中央をスティーブが歩き続ける作品。SSDが壊れてプロジェクトデータが消えてしまいました。
+                    </Work>
+                    <Work title='Chapter Generator' tags={["Lua", "Davinci Resolve"]} imagePath='/works/chapter_generator.png' alt="chapter_generator">
+                        Davinci Resolve でタイムラインマーカーの位置を元に Youtube のチャプター文字列を生成するツール。
+                    </Work>
+                    <Work title='BTC Indicators Dashboard' tags={["Vue.js"]} imagePath='/works/btc-dashboard.png' alt="btc dashboard" url="https://jetbtc.netlify.app">
+                        仮想通貨の指標をまとめて監視できるツール
+                    </Work>
+                    <Work title='SmokeTimer' tags={["Electron", "Vue.js"]} iconType='Github' url="https://github.com/uma0317/SmokeTimer">
+                        喫煙頻度を落とすために作った常時最前面に表示される小さな喫煙タイマー。
+                    </Work>
+                    <Work title='chip8rs' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/chip8rs">
+                        リアルタイムVJシステムです。MIDIキーボードからパラメータ調整可能。
+                    </Work>
+                    <Work title='VJ' tags={["Unity", "MIDI"]} imagePath='/works/vjgif.gif' alt="vj">
+                        リアルタイムVJシステムです。MIDIキーボードからパラメータ調整可能。
+                    </Work>
+                    <Work title='KISS Client' tags={["Swift"]} iconType='Github' url="https://github.com/uma0317/KISS-Client">
+                        KISSのMacクライアントです。Macでのキー入力を無効化しつつサーバーに入力イベントを送るのにてこずりました。
+                    </Work>
+                    <Work title='KISS' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/KISS">
+                        MacからWindowsにキーボード入力できるやつです。こちらはWindows側で動かすサーバーです。MacクライアントからUDP通信でキー入力を受け取れます。
+                    </Work>
+                    <Work title='ere' tags={["Rust", "MIDI"]} iconType='Github' url="https://github.com/uma0317/ere">
+                        Eleven Rackを操作するためにフットコントローラーが欲しかったので、MIDIコントローラから操作できるものを作りました。RustでWin32 API通して登録した座標をクリックする感じです。
+                    </Work>
+                    <Work title='Server Watcher' tags={["Rust", "Vue.js"]} imagePath='/works/server_watcher.png' alt="server_watcher">
+                        サーバリソース監視ツール。サーバ監視のクライアントとサーバ、DashBoardのAPIサーバにRustを使っています。
+                    </Work>
+                    <Work title='motiva' tags={["Vue.js"]} imagePath='/works/motiva.png' alt="motiva">
+                        自己分析をする時、モチベーショングラフを書こうと思ったのですが、紙に書くのが嫌すぎてツールを作りました。PWAです。
+                    </Work>
+                    <Work title='rustatic' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/rustatic">
+                        静的サイトジェネレーターもどき。ブログを始めようと思ったから作りました。
+                    </Work>
+                    <Work title='Rust Line Bot' tags={["Rust", "Heroku"]} imagePath='/works/linebot.jpg' alt="linebot">
+                        Rust製の自作api wrapperを使ってline botを作りました。気温と天気表示、todoタスクを実装しました。
+                    </Work>
+                    <Work title='line-messaging-api-rust' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/line-messaging-api-rust">
+                        Line Messaging API Wrapper。Rust用のSDKが無かったので作りました。
+                    </Work>
+                    <Work title='Gradation' tags={["IndexedDB", "Vue.js"]} imagePath='/works/gradation.gif' alt="gradation">
+                        ProcessingでVJぽい遊びができるツール。Arduinoと連携して遊ぶこともできる
+                    </Work>
+                    <Work title='PWA時間割' tags={["IndexedDB", "Vue.js"]} imagePath='/works/pwa_jikanwari.jpg' alt="pwa_jikanwari">
+                        当時話題になり立てだったPWAとIndexedDBに触れてみたかったので作ってみました。
+                    </Work>
+                    <Work title='Kogera' tags={["Node.js", "Angular.js"]} imagePath='/works/kogera.jpg' alt="Kogera">
+                        Angular製のTwitter Viewer
+                    </Work>
+                    <Work title='Rust-BlockChain' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/bom">
+                        Rustで実装したシンプルなBlockChain
+                    </Work>
+                    <Work title='BOM' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/Rust-BlockChain">
+                        ボイラープレートをCLIで管理するシンプルなRust製ツールです。
+                    </Work>
+                    <Work title='JATTS' tags={["Java"]} imagePath='/works/jatts.jpg' alt="Jatts">
+                        大学の課題において、Javaを使用して特定のキーワードを含むツイートをリアルタイムでストリーミングするアプリケーションを開発しました。
+                    </Work>
+                    <Work title='日本の景色bot' tags={["Ruby"]} iconType='twitter' url='https://twitter.com/jp_scenery'>
+                        日本の景色のライブ写真を呟くTwitter bot。スクレイピングして写真を取ってきます。
+                    </Work>
+                    <Work title='Dream' tags={["Amazon Cognito", "Vue.js"]} imagePath='/works/dream.jpg' alt="Dream">
+                        AWSインターンシップのハッカソンでチーム開発したもの。機械学習によりユーザーが選択した興味のある言葉に関連する職業を提示するサービス。
+                    </Work>
+                </ul>
+            </div>
+        </div>
+    )
+}
