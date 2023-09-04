4633c87e64edb219f6744610584c7cceca5cf066
uma0317 <blog3170@gmail.com>
2023-09-05 05:04:26 +0900

    Fix

--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -13,8 +13,8 @@ type Props = {
 
 
 export default function Work({ title, tags, imagePath, alt, iconType, url, children }: Props) {
-    const tagsEl = tags.map(tag => {
-        return <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 mr-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span>
+    const tagsEl = tags.map((tag, index) => {
+        return <span key={index} className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 mr-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{tag}</span >
     })
     return (
         <li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">Hello, I'm Yuma Yamazaki</h1>
+            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">Hello, I&apos;m Yuma Yamazaki</h1>
             <p className="mt-6 text-lg leading-8">
               関東在住のエンジニアです<br />
               シンプルなモノ、効率化すること、クリエイティブなコトが大好きです
