2f256d9089f81b24c2d11aa642e7e32d8ea8b549
uma0317 <blog3170@gmail.com>
2023-09-05 09:27:36 +0900

    Change Images to webp

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -35,34 +35,34 @@ export default function Home() {
                     <Work title='Portfolio Site' tags={["Next.js", "React", "Tailwind CSS", "TypeScript", "Cloudflare Pages"]} iconType='Github' url="https://github.com/uma0317/portfolio">
                         このサイトです。
                     </Work>
-                    <Work title='neos' tags={["React", "TypeScript", "VScode"]} imagePath='/works/neos.png' alt="neos" onClick={clickImage}>
+                    <Work title='neos' tags={["React", "TypeScript", "VScode"]} imagePath='/works/neos.webp' alt="neos" onClick={clickImage}>
                         感情曲線と脚本を簡単に記述するためのvscode拡張機能。Webview APIを使ってYAMLファイルからプレビュー画面を生成します。
                     </Work>
-                    <Work title='Neoki Beat' tags={["UE5", "MIDI"]} imagePath='/works/sound_game.png' alt="sound_game" onClick={clickImage}>
+                    <Work title='Neoki Beat' tags={["UE5", "MIDI"]} imagePath='/works/sound_game.webp' alt="sound_game" onClick={clickImage}>
                         自作の音ゲー。MIDI で譜面データを作成し、UE5上で読み込んでいます。
                     </Work>
-                    <Work title='All Dash' tags={["UE5"]} imagePath='/works/alldash.png' alt="alldash" onClick={clickImage}>
+                    <Work title='All Dash' tags={["UE5"]} imagePath='/works/alldash.webp' alt="alldash" onClick={clickImage}>
                         シンプルな2D横スクロールアクションゲーム。T-Rex Dinosaur Gameのような感じです。
                     </Work>
-                    <Work title='Keiba' tags={["UE5", "blender", "Node.js"]} imagePath='/works/keiba.png' alt="keiba" onClick={clickImage}>
+                    <Work title='Keiba' tags={["UE5", "blender", "Node.js"]} imagePath='/works/keiba.webp' alt="keiba" onClick={clickImage}>
                         競馬風レースゲーム。(Todo)Youtube チャットをストリーミングしてリアルタイムで参加・投票ができるようにする予定です。
                     </Work>
-                    <Work title='bl2pa' tags={["Minecraft", "blender", "Node.js", "Python"]} imagePath='/works/bl2pa.png' alt="bl2pa" onClick={clickImage}>
+                    <Work title='bl2pa' tags={["Minecraft", "blender", "Node.js", "Python"]} imagePath='/works/bl2pa.webp' alt="bl2pa" onClick={clickImage}>
                         blender で 3Dモデルを点群に変換しPythonで頂点およびカラー情報を計算、その後パーティクルコマンドに変換しMinecraft で再生するツール。複雑なアニメーションにも対応。
                     </Work>
-                    <Work title='particle-video' tags={["Minecraft", "Node.js"]} imagePath='/works/particle_video.png' alt="particle-video" onClick={clickImage}>
+                    <Work title='particle-video' tags={["Minecraft", "Node.js"]} imagePath='/works/particle_video.webp' alt="particle-video" onClick={clickImage}>
                         画像や動画を Minecraft のパーティクルコマンドに変換し、Minecraft 上で再生するツール。
                     </Work>
                     <Work title='自動字幕生成ツール' tags={["Python", "Lua", "Davinci Resolve", "Whisper"]} iconType='Github'>
                         Whisperで文字起こしをして、Davicni Resolve のテキストを自動で生成するスクリプト
                     </Work>
-                    <Work title='Audio Reactive' tags={["UE5", "blender"]} imagePath='/works/ue_sound_minecraft.png' alt="ue_sound_minecraft" onClick={clickImage}>
+                    <Work title='Audio Reactive' tags={["UE5", "blender"]} imagePath='/works/ue_sound_minecraft.webp' alt="ue_sound_minecraft" onClick={clickImage}>
                         UE5 でサウンドに合わせてオブジェクトを生成し、その中央をスティーブが歩き続ける作品。SSDが壊れてプロジェクトデータが消えてしまいました。
                     </Work>
-                    <Work title='Chapter Generator' tags={["Lua", "Davinci Resolve"]} imagePath='/works/chapter_generator.png' alt="chapter_generator" onClick={clickImage}>
+                    <Work title='Chapter Generator' tags={["Lua", "Davinci Resolve"]} imagePath='/works/chapter_generator.webp' alt="chapter_generator" onClick={clickImage}>
                         Davinci Resolve でタイムラインマーカーの位置を元に Youtube のチャプター文字列を生成するツール。
                     </Work>
-                    <Work title='BTC Indicators Dashboard' tags={["Vue.js", "Nuxt.js"]} imagePath='/works/btc-dashboard.png' alt="btc dashboard" url="https://jetbtc.netlify.app" onClick={clickImage}>
+                    <Work title='BTC Indicators Dashboard' tags={["Vue.js", "Nuxt.js"]} imagePath='/works/btc-dashboard.webp' alt="btc dashboard" url="https://jetbtc.netlify.app" onClick={clickImage}>
                         仮想通貨の指標をまとめて監視できるツール
                     </Work>
                     <Work title='SmokeTimer' tags={["Electron", "Vue.js"]} iconType='Github' url="https://github.com/uma0317/SmokeTimer" onClick={clickImage}>
@@ -83,16 +83,16 @@ export default function Home() {
                     <Work title='ere' tags={["Rust", "MIDI"]} iconType='Github' url="https://github.com/uma0317/ere" onClick={clickImage}>
                         Eleven Rackを操作するためにフットコントローラーが欲しかったので、MIDIコントローラから操作できるものを作りました。RustでWin32 API通して登録した座標をクリックする感じです。
                     </Work>
-                    <Work title='Server Watcher' tags={["Rust", "Vue.js"]} imagePath='/works/server_watcher.png' alt="server_watcher" onClick={clickImage}>
+                    <Work title='Server Watcher' tags={["Rust", "Vue.js"]} imagePath='/works/server_watcher.webp' alt="server_watcher" onClick={clickImage}>
                         サーバリソース監視ツール。サーバ監視のクライアントとサーバ、DashBoardのAPIサーバにRustを使っています。
                     </Work>
-                    <Work title='motiva' tags={["Vue.js"]} imagePath='/works/motiva.png' alt="motiva" url="https://motiva.netlify.app/" onClick={clickImage}>
+                    <Work title='motiva' tags={["Vue.js"]} imagePath='/works/motiva.webp' alt="motiva" url="https://motiva.netlify.app/" onClick={clickImage}>
                         自己分析をする時、モチベーショングラフを書こうと思ったのですが、紙に書くのが嫌すぎてツールを作りました。PWAです。
                     </Work>
                     <Work title='rustatic' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/rustatic" onClick={clickImage}>
                         静的サイトジェネレーターもどき。ブログを始めようと思ったから作りました。
                     </Work>
-                    <Work title='Rust Line Bot' tags={["Rust", "Heroku"]} imagePath='/works/linebot.jpg' alt="linebot" onClick={clickImage}>
+                    <Work title='Rust Line Bot' tags={["Rust", "Heroku"]} imagePath='/works/linebot.webp' alt="linebot" onClick={clickImage}>
                         Rust製の自作api wrapperを使ってline botを作りました。気温と天気表示、todoタスクを実装しました。
                     </Work>
                     <Work title='line-messaging-api-rust' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/line-messaging-api-rust" onClick={clickImage}>
@@ -101,10 +101,10 @@ export default function Home() {
                     <Work title='Gradation' tags={["IndexedDB", "Vue.js"]} imagePath='/works/gradation.gif' alt="gradation" url="https://github.com/uma0317/gradation" onClick={clickImage}>
                         ProcessingでVJぽい遊びができるツール。Arduinoと連携して遊ぶこともできる
                     </Work>
-                    <Work title='PWA時間割' tags={["IndexedDB", "Vue.js"]} imagePath='/works/pwa_jikanwari.jpg' alt="pwa_jikanwari" onClick={clickImage}>
+                    <Work title='PWA時間割' tags={["IndexedDB", "Vue.js"]} imagePath='/works/pwa_jikanwari.webp' alt="pwa_jikanwari" onClick={clickImage}>
                         当時話題になり立てだったPWAとIndexedDBに触れてみたかったので作ってみました。
                     </Work>
-                    <Work title='Kogera' tags={["Node.js", "Angular.js"]} imagePath='/works/kogera.jpg' alt="Kogera" onClick={clickImage}>
+                    <Work title='Kogera' tags={["Node.js", "Angular.js"]} imagePath='/works/kogera.webp' alt="Kogera" onClick={clickImage}>
                         Angular製のTwitter Viewer
                     </Work>
                     <Work title='Rust-BlockChain' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/bom" onClick={clickImage}>
@@ -113,13 +113,13 @@ export default function Home() {
                     <Work title='BOM' tags={["Rust"]} iconType='Github' url="https://github.com/uma0317/Rust-BlockChain" onClick={clickImage}>
                         ボイラープレートをCLIで管理するシンプルなRust製ツールです。
                     </Work>
-                    <Work title='JATTS' tags={["Java"]} imagePath='/works/jatts.jpg' alt="Jatts" onClick={clickImage}>
+                    <Work title='JATTS' tags={["Java"]} imagePath='/works/jatts.webp' alt="Jatts" onClick={clickImage}>
                         大学の課題において、Javaを使用して特定のキーワードを含むツイートをリアルタイムでストリーミングするアプリケーションを開発しました。
                     </Work>
                     <Work title='日本の景色bot' tags={["Ruby"]} iconType='twitter' url='https://twitter.com/jp_scenery'>
                         日本の景色のライブ写真を呟くTwitter bot。スクレイピングして写真を取ってきます。
                     </Work>
-                    <Work title='Dream' tags={["Amazon Cognito", "Vue.js"]} imagePath='/works/dream.jpg' alt="Dream" onClick={clickImage}>
+                    <Work title='Dream' tags={["Amazon Cognito", "Vue.js"]} imagePath='/works/dream.webp' alt="Dream" onClick={clickImage}>
                         AWSインターンシップのハッカソンでチーム開発したもの。機械学習によりユーザーが選択した興味のある言葉に関連する職業を提示するサービス。
                     </Work>
                 </ul>
